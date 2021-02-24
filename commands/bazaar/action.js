bf6fe2f7132f77b9a656f6eb19238932bb3e1a32
Will Sullivan <furtivespy@gmail.com>
2021-02-23 16:20:05 -0800

    basic version of actions

--- commands/bazaar/action.js
+++ commands/bazaar/action.js
@@ -2,6 +2,7 @@ const Command = require('../../base/Command.js')
 const Discord = require('discord.js')
 const _ = require('lodash')
 const bazaarData = require('../../db/bazaar.js')
+const BazaarFormatter = require('../../modules/BazaarFormatter.js')
 
 
 class Action extends Command {
@@ -98,10 +99,58 @@ class Action extends Command {
                     this.client.TryExecuteCommand("bazaar-game", message, [])
                 }
                 if (collected.first().emoji.name == '⭐') {
-                    message.channel.send('do star!')
+                    const objectiveEmbed = new Discord.MessageEmbed().setColor(386945).setTitle("Which Objective Are You Claiming").setTimestamp()
+                    objectiveEmbed.addField(`Objectives`,`
+:regional_indicator_a::arrow_right:  ${BazaarFormatter.objectiveFormat(gameData.objectiveA[0])} (${gameData.objectiveA.length})
+:regional_indicator_b::arrow_right:  ${BazaarFormatter.objectiveFormat(gameData.objectiveB[0])} (${gameData.objectiveB.length})
+:regional_indicator_c::arrow_right:  ${BazaarFormatter.objectiveFormat(gameData.objectiveC[0])} (${gameData.objectiveC.length})
+:regional_indicator_d::arrow_right:  ${BazaarFormatter.objectiveFormat(gameData.objectiveD[0])} (${gameData.objectiveD.length})`)
+                    await msg.suppressEmbeds(true)
+                    await msg.reactions.removeAll()
+                    await msg.edit(objectiveEmbed)
+                    msg.react("🇦").then(r => msg.react("🇧").then(r => msg.react("🇨").then(r => msg.react("🇩"))))
+
+                    try {
+                        var collectedObjective = await msg.awaitReactions((reaction, user) => user.id == currentPlayer 
+                            && (reaction.emoji.name == '🇦' || reaction.emoji.name == '🇧' || reaction.emoji.name == '🇨' || reaction.emoji.name == '🇩'),
+                            { max: 1, time: 30000 })
+                    } catch {
+                        msg.edit('No reaction after 30 seconds, turn attempt canceled - try again when ready');
+                        msg.suppressEmbeds(true)
+                        msg.reactions.removeAll()
+                        return
+                    }
+
+                    await msg.edit(`You Picked ${collectedObjective.first().emoji.name}`)
+                    msg.suppressEmbeds(true)
+                    msg.reactions.removeAll()
+
                 }
                 if (collected.first().emoji.name == '🛍') {
-                    message.channel.send('do bag!')
+
+                    const exchangeEmbed = new Discord.MessageEmbed().setColor(386945).setTitle("Which Swap do you want to make").setTimestamp()
+                    exchangeEmbed.addField(`Exchanges`, `${BazaarFormatter.bazaarFormat([...gameData.theBazaar[0].trades,...gameData.theBazaar[1].trades])}`)
+                    await msg.suppressEmbeds(true)
+                    await msg.reactions.removeAll()
+                    await msg.edit(exchangeEmbed)
+                    msg.react("1️⃣").then(r => msg.react("2️⃣").then(r => msg.react("3️⃣").then(r => msg.react("4️⃣").then(r => msg.react("5️⃣")
+                    .then(r => msg.react("6️⃣").then(r => msg.react("7️⃣").then(r => msg.react("8️⃣").then(r => msg.react("9️⃣").then(r => msg.react("🔟"))))))))))
+
+                    try {
+                        var collectedSwap = await msg.awaitReactions((reaction, user) => user.id == currentPlayer 
+                            && (reaction.emoji.name == '1️⃣' || reaction.emoji.name == '2️⃣' || reaction.emoji.name == '3️⃣' || reaction.emoji.name == '4️⃣' || reaction.emoji.name == '5️⃣'
+                             || reaction.emoji.name == '6️⃣' || reaction.emoji.name == '7️⃣' || reaction.emoji.name == '8️⃣' || reaction.emoji.name == '9️⃣' || reaction.emoji.name == '🔟'),
+                            { max: 1, time: 60000 })
+                    } catch {
+                        msg.edit('No reaction after 60 seconds, turn attempt canceled - try again when ready');
+                        msg.suppressEmbeds(true)
+                        msg.reactions.removeAll()
+                        return
+                    }
+
+                    await msg.edit(`You Picked ${collectedSwap.first().emoji.name}`)
+                    msg.suppressEmbeds(true)
+                    msg.reactions.removeAll()
                 }
 
                 
