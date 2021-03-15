01b4db8b798870e018fbb4066dd7af987e44fc55
Will Sullivan <furtivespy@gmail.com>
2021-03-15 12:35:54 -0700

    add discard to help

--- commands/cards/help.js
+++ commands/cards/help.js
@@ -28,13 +28,15 @@ class Help extends Command {
             // **Shuffle** - Shuffles a deck (newly added decks will not be shuffled at first)
             // **Flip** - Flips the top card(s) off the deck
             // **Deal** - Nothing Yet`)
-            cardsEmbed.addField(`Cards`, `Basic command that shows active card decks in the channel`, true)
-            cardsEmbed.addField(`New`, `Add a new deck to the channel (use without parameters to get detailed help)`, true)
+            cardsEmbed.addField(`Cards`, `Basic command that shows active card decks in the channel\n \`&cards\``, true)
+            cardsEmbed.addField(`New`, `Add a new deck to the channel (use without parameters to get detailed help)\n \`&cards new\``, true)
             cardsEmbed.addField(`Remove`, `Removes a deck from the channel\n \`&cards remove deckname\``, true)
-            cardsEmbed.addField(`Shuffle`, `Shuffles a deck (newly added decks will not be shuffled at first)`, true)
+            cardsEmbed.addField(`Shuffle`, `Shuffles a deck (newly added decks will not be shuffled at first)\n \`&cards shuffle deckname\``, true)
             cardsEmbed.addField(`Flip`, `Flips the top card(s) off the deck\n \`&cards flip deckname [number]\``, true)
-            cardsEmbed.addField(`Deal`, `Nothing Yet`, true)
-            cardsEmbed.addField(`Reminder`, `All commands for cards game start with &cards`)
+            cardsEmbed.addField(`Draw`, `Draw a card from a deck into your hand\n \`&cards draw deckname\``, true)
+            cardsEmbed.addField(`Discard`, `Discard a card\n \`&cards discard\``, true)
+            //cardsEmbed.addField(`Deal`, `Nothing Yet\n \`&cards\``, true)
+            cardsEmbed.addField(`Reminder`, `All commands for card game start with \`&cards\` or \`&c\``)
             await message.channel.send(cardsEmbed);
         } catch (e) {
             this.client.logger.error(e, __filename.slice(__dirname.length + 1))
