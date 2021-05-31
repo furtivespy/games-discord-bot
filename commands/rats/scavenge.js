b3b1c9bfdc58d778c0d8be193348da24442b0465
Will Sullivan <furtivespy@gmail.com>
2021-05-31 14:02:09 -0700

    update emoji

--- commands/rats/scavenge.js
+++ commands/rats/scavenge.js
@@ -2,7 +2,7 @@ const Command = require('../../base/Command.js')
 const Discord = require('discord.js')
 const Roller = require('roll')
 const Sample = require('lodash/sample')
-const diceSides = [":die1:",":die2:",":die3:",":die4:",":die5:",":die6:"]
+const diceSides = ["<:die1:790027072998342666>","<:die2:790028311756668960>","<:die3:790028312167841803>","<:die4:790028312348065842>","<:die5:790028312386076713>","<:die6:790028312495128616>"]
 
 class Scavenge extends Command {
     constructor(client){
