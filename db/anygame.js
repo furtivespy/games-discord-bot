a28ba14f57c425bc6e59f615749220cf6d7e8cc8
Will Sullivan <furtivespy@gmail.com>
2023-02-16 21:24:28 -0500

    add QE decks

--- db/anygame.js
+++ db/anygame.js
@@ -23,6 +23,9 @@ class GameDatabase {
         [ "Brass Birmingham - Wild Industry", "brass-wild-industry"],
         [ "Blue Moon City", "blue-moon-city"],
         [ "Guild of Merchant Explorers - Explore Deck", "gome-explorer"],
+        [ "Quantitative Easing (QE) - Industry", "qe-industry"],
+        [ "Quantitative Easing (QE) - Company 3-4 Players", "qe-company-3"],
+        [ "Quantitative Easing (QE) - Company 5 Players", "qe-company-5"],
     ]
 
     defaultGameData = {
@@ -175,6 +178,12 @@ class GameDatabase {
                 return this.createCardFromObjList(deckName, "A", blueMoonCity)
             case "gome-explorer":
                 return this.createCardFromObjList(deckName, "B", guildExplore)
+            case "qe-industry":
+                return this.createCardFromObjList(deckName, "B", qeIndustry)
+            case "qe-company-3":
+                return this.createCardFromObjList(deckName, "B", qeCompany3)
+            case "qe-company-5":
+                return this.createCardFromObjList(deckName, "B", qeCompany5)
             default:
                 return []
         }
@@ -749,5 +758,50 @@ const guildExplore = [
     {name: "Era I", type: "Explore", suit: "none", url: "https://furtivespy.com/images/guild/explore_1_1.png"},
     {name: "Era II", type: "Explore", suit: "none", url: "https://furtivespy.com/images/guild/explore_1_2.png"},
     {name: "Era III", type: "Explore", suit: "none", url: "https://furtivespy.com/images/guild/explore_1_3.png"},
+]
+
+const qeIndustry = [
+    {name: "Agriculture", type: "Industry", suit: "none", url: "https://furtivespy.com/images/qe/agriculture.jpg"},
+    {name: "Housing", type: "Industry", suit: "none", url: "https://furtivespy.com/images/qe/housing.jpg"},
+    {name: "Government", type: "Industry", suit: "none", url: "https://furtivespy.com/images/qe/government.jpg"},
+    {name: "Finance", type: "Industry", suit: "none", url: "https://furtivespy.com/images/qe/finance.jpg"},
+    {name: "Manufacturing", type: "Industry", suit: "none", url: "https://furtivespy.com/images/qe/manufacturing.jpg"},
+]
+
+const qeCompany3 = [
+    {name: "Agriculture - 2", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-a.jpg"},
+    {name: "Finance - 4", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-f.jpg"},
+    {name: "Housing - 3", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-h.jpg"},
+    {name: "Manufacturing - 1", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-m.jpg"},
+    {name: "Agriculture - 1", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-a.jpg"},
+    {name: "Finance - 3", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-f.jpg"},
+    {name: "Housing - 2", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-h.jpg"},
+    {name: "Manufacturing - 4", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-m.jpg"},
+    {name: "Agriculture - 3", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-a.jpg"},
+    {name: "Finance - 1", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-f.jpg"},
+    {name: "Housing - 4", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-h.jpg"},
+    {name: "Manufacturing - 2", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-m.jpg"},
+    {name: "Agriculture - 4", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-a.jpg"},
+    {name: "Finance - 2", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-f.jpg"},
+    {name: "Housing - 1", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-h.jpg"},
+    {name: "Manufacturing - 3", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-m.jpg"},
+]
+
+const qeCompany5 = [
+    {name: "Agriculture - 2", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-a.jpg"},
+    {name: "Housing - 3", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-h.jpg"},
+    {name: "Government - 4", type: "CN", suit: "CN", url: "https://furtivespy.com/images/qe/CN-g.jpg"},
+    {name: "Finance - 3", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-f.jpg"},
+    {name: "Housing - 2", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-h.jpg"},
+    {name: "Manufacturing - 4", type: "EU", suit: "EU", url: "https://furtivespy.com/images/qe/EU-m.jpg"},
+    {name: "Housing - 4", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-h.jpg"},
+    {name: "Manufacturing - 2", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-m.jpg"},
+    {name: "Government - 3", type: "JP", suit: "JP", url: "https://furtivespy.com/images/qe/JP-g.jpg"},
+    {name: "Agriculture - 3", type: "UK", suit: "UK", url: "https://furtivespy.com/images/qe/UK-a.jpg"},
+    {name: "Finance - 4", type: "UK", suit: "UK", url: "https://furtivespy.com/images/qe/UK-f.jpg"},
+    {name: "Government - 2", type: "UK", suit: "UK", url: "https://furtivespy.com/images/qe/UK-g.jpg"},
+    {name: "Agriculture - 4", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-a.jpg"},
+    {name: "Finance - 2", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-f.jpg"},
+    {name: "Manufacturing - 3", type: "US", suit: "US", url: "https://furtivespy.com/images/qe/US-m.jpg"},
 ]
 module.exports = new GameDatabase();
