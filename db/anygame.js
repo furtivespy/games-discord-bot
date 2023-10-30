a8fb717ee9e93422ffecbeb68aaa9b1a44822e9d
Will Sullivan <furtivespy@gmail.com>
2023-10-29 21:56:35 -0400

    add new deck

--- db/anygame.js
+++ db/anygame.js
@@ -37,6 +37,7 @@ class GameDatabase {
         [ "Heat - Starting Hand", "heat-starting"],
         [ "Heat - Upgrades", "heat-upgrades"],
         [ "Heat - All Cards", "heat-all"],
+        [ "Empire's End - Disaster", "empire-disaster"],
     ]
 
     defaultGameData = {
@@ -230,6 +231,8 @@ class GameDatabase {
                 return this.createCardFromObjList(deckName, "B", heatUpgrades)
             case "heat-all":
                 return this.createCardFromObjList(deckName, "B", [...heatGeneric, ...heatBase, ...heatUpgrades])
+            case "empire-disaster":
+                return this.createCardFromObjList(deckName, "B", empiresEnd)
             default:
                 return []
         }
@@ -1103,6 +1106,75 @@ const heatUpgrades = [
     {name: `Wings (3)`, value: 3, type: `Upgrade`, suit: `d`, url: `https://furtivespy.com/images/heat/wings3.png`},
     {name: `Wings (4)`, value: 4, type: `Upgrade`, suit: `d`, url: `https://furtivespy.com/images/heat/wings4.png`},
     {name: `Wings (6)`, value: 6, type: `Upgrade`, suit: `d`, url: `https://furtivespy.com/images/heat/wings6.png`},
+]
+
+const empiresEnd = [
+    {name: `1`, value: 1, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_0.png`},
+    {name: `2`, value: 2, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_1.png`},
+    {name: `3`, value: 3, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_2.png`},
+    {name: `4`, value: 4, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_3.png`},
+    {name: `5`, value: 5, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_4.png`},
+    {name: `6`, value: 6, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_5.png`},
+    {name: `7`, value: 7, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_6.png`},
+    {name: `8`, value: 8, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_7.png`},
+    {name: `9`, value: 9, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_8.png`},
+    {name: `10`, value: 10, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_0_9.png`},
+    {name: `11`, value: 11, type: `Fire`, suit: `Fire`, url: `https://furtivespy.com/images/empiresend/empires-end_1_0.png`},
+    {name: `1`, value: 1, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_1.png`},
+    {name: `2`, value: 2, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_2.png`},
+    {name: `3`, value: 3, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_3.png`},
+    {name: `4`, value: 4, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_4.png`},
+    {name: `5`, value: 5, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_5.png`},
+    {name: `6`, value: 6, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_6.png`},
+    {name: `7`, value: 7, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_7.png`},
+    {name: `8`, value: 8, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_8.png`},
+    {name: `9`, value: 9, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_1_9.png`},
+    {name: `10`, value: 10, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_2_0.png`},
+    {name: `11`, value: 11, type: `Revolt`, suit: `Revolt`, url: `https://furtivespy.com/images/empiresend/empires-end_2_1.png`},
+    {name: `1`, value: 1, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_2.png`},
+    {name: `2`, value: 2, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_3.png`},
+    {name: `3`, value: 3, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_4.png`},
+    {name: `4`, value: 4, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_5.png`},
+    {name: `5`, value: 5, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_6.png`},
+    {name: `6`, value: 6, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_7.png`},
+    {name: `7`, value: 7, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_8.png`},
+    {name: `8`, value: 8, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_2_9.png`},
+    {name: `9`, value: 9, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_3_0.png`},
+    {name: `10`, value: 10, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_3_1.png`},
+    {name: `11`, value: 11, type: `Barbarians`, suit: `Barbarians`, url: `https://furtivespy.com/images/empiresend/empires-end_3_2.png`},
+    {name: `1`, value: 1, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_3.png`},
+    {name: `2`, value: 2, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_4.png`},
+    {name: `3`, value: 3, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_5.png`},
+    {name: `4`, value: 4, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_6.png`},
+    {name: `5`, value: 5, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_7.png`},
+    {name: `6`, value: 6, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_8.png`},
+    {name: `7`, value: 7, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_3_9.png`},
+    {name: `8`, value: 8, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_4_0.png`},
+    {name: `9`, value: 9, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_4_1.png`},
+    {name: `10`, value: 10, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_4_2.png`},
+    {name: `11`, value: 11, type: `Famine`, suit: `Famine`, url: `https://furtivespy.com/images/empiresend/empires-end_4_3.png`},
+    {name: `1`, value: 1, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_4.png`},
+    {name: `2`, value: 2, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_5.png`},
+    {name: `3`, value: 3, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_6.png`},
+    {name: `4`, value: 4, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_7.png`},
+    {name: `5`, value: 5, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_8.png`},
+    {name: `6`, value: 6, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_4_9.png`},
+    {name: `7`, value: 7, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_5_0.png`},
+    {name: `8`, value: 8, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_5_1.png`},
+    {name: `9`, value: 9, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_5_2.png`},
+    {name: `10`, value: 10, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_5_3.png`},
+    {name: `11`, value: 11, type: `Flood`, suit: `Flood`, url: `https://furtivespy.com/images/empiresend/empires-end_5_4.png`},
+    {name: `1`, value: 1, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_5_5.png`},
+    {name: `2`, value: 2, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_5_6.png`},
+    {name: `3`, value: 3, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_5_7.png`},
+    {name: `4`, value: 4, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_5_8.png`},
+    {name: `5`, value: 5, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_5_9.png`},
+    {name: `6`, value: 6, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_0.png`},
+    {name: `7`, value: 7, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_1.png`},
+    {name: `8`, value: 8, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_2.png`},
+    {name: `9`, value: 9, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_3.png`},
+    {name: `10`, value: 10, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_4.png`},
+    {name: `11`, value: 11, type: `Blight`, suit: `Blight`, url: `https://furtivespy.com/images/empiresend/empires-end_6_5.png`},
 ]
 
 module.exports = new GameDatabase();
