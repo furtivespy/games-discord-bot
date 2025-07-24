8ece6ca3a9564adbeb3f8b421b2a59c0b8b75545
WillSwannack <porousnapkin@gmail.com>
2025-07-24 13:54:29 -0700

    Remove parens data from names since it's not necessary for display.

--- db/decks/spectral.js
+++ db/decks/spectral.js
@@ -1,24 +1,24 @@
 
 
 const spectralCards = [
-  {name: "Two Diagonal Glyph: A (Skull)", type: "Skull", suit: "A", value: 1},
-  {name: "Corner Quadrant Glyph: A (Diamond)", type: "Diamond", suit: "A", value: 1},
-  {name: "Horizontal Reflection Glyph: A (Diamond)", type: "Diamond", suit: "A", value: 1},
-  {name: "Two Clockwise Glyph: A (Diamond)", type: "Diamond", suit: "A", value: 1},
+  {name: "Two Diagonal Glyph: A", type: "Skull", suit: "A", value: 1},
+  {name: "Corner Quadrant Glyph: A", type: "Diamond", suit: "A", value: 1},
+  {name: "Horizontal Reflection Glyph: A", type: "Diamond", suit: "A", value: 1},
+  {name: "Two Clockwise Glyph: A", type: "Diamond", suit: "A", value: 1},
 
-  {name: "Two Diagonal Glyph: B (Diamond)", type: "Diamond", suit: "B", value: 2},
-  {name: "Corner Quadrant Glyph: B (Skull)", type: "Skull", suit: "B", value: 2},
-  {name: "Horizontal Reflection Glyph: B (Diamond)", type: "Diamond", suit: "B", value: 2},
-  {name: "Two Clockwise Glyph: B (Diamond)", type: "Diamond", suit: "B", value: 2},
+  {name: "Two Diagonal Glyph: B", type: "Diamond", suit: "B", value: 2},
+  {name: "Corner Quadrant Glyph: B", type: "Skull", suit: "B", value: 2},
+  {name: "Horizontal Reflection Glyph: B", type: "Diamond", suit: "B", value: 2},
+  {name: "Two Clockwise Glyph: B", type: "Diamond", suit: "B", value: 2},
 
-  {name: "Two Diagonal Glyph: C (Diamond)", type: "Diamond", suit: "C", value: 3},
-  {name: "Corner Quadrant Glyph: C (Diamond)", type: "Diamond", suit: "C", value: 3},
-  {name: "Horizontal Reflection Glyph: C (Diamond)", type: "Diamond", suit: "C", value: 3},
-  {name: "Two Clockwise Glyph: C (Skull)", type: "Skull", suit: "C", value: 3},
+  {name: "Two Diagonal Glyph: C", type: "Diamond", suit: "C", value: 3},
+  {name: "Corner Quadrant Glyph: C", type: "Diamond", suit: "C", value: 3},
+  {name: "Horizontal Reflection Glyph: C", type: "Diamond", suit: "C", value: 3},
+  {name: "Two Clockwise Glyph: C", type: "Skull", suit: "C", value: 3},
   
-  {name: "Two Diagonal Glyph: D (Diamond)", type: "Diamond", suit: "D", value: 4},
-  {name: "Corner Quadrant Glyph: D (Diamond)", type: "Diamond", suit: "D", value: 4},
-  {name: "Horizontal Reflection Glyph: D (Skull)", type: "Skull", suit: "D", value: 4},
-  {name: "Two Clockwise Glyph: D (Diamond)", type: "Diamond", suit: "D", value: 4},
+  {name: "Two Diagonal Glyph: D", type: "Diamond", suit: "D", value: 4},
+  {name: "Corner Quadrant Glyph: D", type: "Diamond", suit: "D", value: 4},
+  {name: "Horizontal Reflection Glyph: D", type: "Skull", suit: "D", value: 4},
+  {name: "Two Clockwise Glyph: D", type: "Diamond", suit: "D", value: 4},
 ];
 module.exports = spectralCards;
