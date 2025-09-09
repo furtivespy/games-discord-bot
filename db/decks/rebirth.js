862c2338184eae0c42df9feb550c5ebe0458531b
Will Sullivan <furtivespy@gmail.com>
2025-09-09 17:02:15 -0400

    rebirth deck

---
+++ db/decks/rebirth.js
@@ -0,0 +1,285 @@
+const rebirth = [
+  {
+    value: 5,
+    name: "Glasgow Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_0.png",
+  },
+  {
+    value: 5,
+    name: "Edinburgh Castle",
+    description:
+      "Occupy the most spaces adjacent to Edinburgh Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_2.png",
+  },
+  {
+    value: 5,
+    name: "Aberdeen Castle",
+    description:
+      "Occupy the most spaces adjacent to Aberdeen Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_3.png",
+  },
+  {
+    value: 5,
+    name: "Perth Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Perth Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_4.png",
+  },
+  {
+    value: 5,
+    name: "Eilean Donan Castle",
+    description:
+      "Occupy the most spaces adjacent to Eilean Donan Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_5.png",
+  },
+  {
+    value: 5,
+    name: "Edinburgh Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Edinburgh Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_6.png",
+  },
+  {
+    value: 5,
+    name: "Most Ports",
+    description:
+      "Occupy the most Port spaces. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_7.png",
+  },
+  {
+    value: 5,
+    name: "The English Border",
+    description:
+      "Occupy the most English border spaces. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_8.png",
+  },
+  {
+    value: 5,
+    name: "Most Castles",
+    description:
+      "Control the most Castles on the board. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_0_9.png",
+  },
+  {
+    value: 5,
+    name: "Blair Castle",
+    description:
+      "Occupy the most spaces adjacent to Blair Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_0.png",
+  },
+  {
+    value: 5,
+    name: "Dumfries Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Dumfries Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_2.png",
+  },
+  {
+    value: 5,
+    name: "Invergarry Castle",
+    description:
+      "Occupy the most spaces adjacent to Invergarry Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_3.png",
+  },
+  {
+    value: 5,
+    name: "Longest Coastline",
+    description:
+      "Occupy the longest sequence of spaces on the coastline. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_4.png",
+  },
+  {
+    value: 5,
+    name: "Medium Settlements Presence",
+    description:
+      "Have the most Medium Settlements, occupy at least 1 space. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_5.png",
+  },
+  {
+    value: 5,
+    name: "Most Energy Farms",
+    description:
+      "Have the most Energy Farm tiles. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_6.png",
+  },
+  {
+    value: 5,
+    name: "Inveraray Castle",
+    description:
+      "Occupy the most spaces adjacent to Inveraray Castle. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_7.png",
+  },
+  {
+    value: 5,
+    name: "Most Food Farms",
+    description:
+      "Have the most farm clusters of 2 or more. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_8.png",
+  },
+  {
+    value: 5,
+    name: "Inverness Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Inverness Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_1_9.png",
+  },
+  {
+    value: 5,
+    name: "Small Settlements Ownership",
+    description:
+      "Have the most Small Settlements. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_0.png",
+  },
+  {
+    value: 5,
+    name: "Largest Energy Farm",
+    description:
+      "Have the largest single Energy Farm cluster. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_1.png",
+  },
+  {
+    value: 5,
+    name: "Large Settlements Presence",
+    description:
+      "Have the most Large Settlements, you must occupy at least 1 space. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_2.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 5,
+    name: "Most Cathedrals",
+    description:
+      "Have the most Cathedrals on the board. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_4.png",
+  },
+  {
+    value: 5,
+    name: "Oban Cathedral",
+    description:
+      "Occupy the most spaces adjacent to Oban Cathedral. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_5.png",
+  },
+  {
+    value: 5,
+    name: "Largest Food Farm",
+    description:
+      "Have the largest single food farm cluster. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_6.png",
+  },
+  {
+    value: 5,
+    name: "Medium Settlements Ownership",
+    description:
+      "Fully occupy the most Medium Settlements. (Ties also satisfy this condition.)",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_7.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+  {
+    value: 3,
+    name: "Cathedral Bonus",
+    description: "3 bonus points.",
+    type: "Mission",
+    suit: "Mission",
+    url: "https://furtivespy.com/images/rebirth/rebirth_2_3.png",
+  },
+];
+
+module.exports = rebirth;
